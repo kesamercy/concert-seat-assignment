290fc741f1ab99061b3cf9920d5359920cb0796a
Nekesa Mercy <kesamercy@gmail.com>
2020-08-10 18:10:37 -0400

    #1 parsed the json file and user can be routed to respective venue page

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -1,17 +1,31 @@
 <?php
 class HomepageCest
 {
-    public function venueButtonsPresent(AcceptanceTester $I)
+    public function homePageElements(AcceptanceTester $I)
     {
         $I->amOnPage('/');
         $I->see('Home');
-        $I->see('Madison');
-        $I->see('Kenedy');
-        $I->see('Criscross');
-
+        
+        
+    }
+    public function venuneButtonsRouteValidation(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
         $I->click('Madison');
+        $I->amOnPage('/parse-json');
+        $I->see('Madison');
+
+        $I->click('back');
+
+        $I->amOnPage('/');
         $I->click('Kenedy');
+        $I->amOnPage('/parse-json');
+        $I->see('Kenedy');
+
+        $I->click('back');
         $I->click('Criscross');
+        $I->amOnPage('/parse-json');
+        $I->see('Criscross');
     }
 }
                 
