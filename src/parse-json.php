be215deba8117416b62b07451532d09c6cf75f0d
Nekesa Mercy <kesamercy@gmail.com>
2020-08-10 10:55:37 -0400

    issue#1 implemented acceptance tests to check for vennue buttons

---
+++ src/parse-json.php
@@ -0,0 +1,7 @@
+<? php
+
+// check if the file exists 
+// check if the file has content 
+// check if the file is formatted correct?
+// parse the input of the file - based on the standard expected.
+?>
--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -1,11 +1,17 @@
 <?php
-
-class FirstCest
+class HomepageCest
 {
-    public function frontpageWorks(AcceptanceTester $I)
+    public function venueButtonsPresent(AcceptanceTester $I)
     {
         $I->amOnPage('/');
         $I->see('Home');
+        $I->see('Madison');
+        $I->see('Kenedy');
+        $I->see('Crisscross');
+
+        $I->click('Madison');
+        $I->click('Kenedy');
+        $I->click('Criscross');
     }
 }
                 
